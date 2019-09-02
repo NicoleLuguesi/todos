75e99663d62f0e77207de6367a40abc77daac6e2
Nicole Luguesi <nicoleluguesi.student@careerdevs.com>
2019-09-02 09:07:24 -0400

    removed for loops

--- todos.js
+++ todos.js
@@ -21,23 +21,35 @@ var todoList = {
         var completedTodos = 0;
 
         // Get number of completed todos.
-        for (var i = 0; i < totalTodos; i++) {
-            if (this.todos[i].completed ===  true) {
+        this.todos.forEach(function(todo) {
+            if (todo.completed === true) {
                 completedTodos++;
             }
-        }
+        });
 
         // Case 1: If everything's true, make everything
-        if (completedTodos === totalTodos) {
-            for (var i = 0; i < totalTodos; i++) {
-                this.todos[i].completed = false;
+        //  if (completedTodos === totalTodos) {
+        //     this.todos.forEach(function(todo){
+        //     todo.completed = false;
+        // });
+
+        //     // Case 2: Otherwise, make everything true.
+        // } else {
+        //     this.todos.forEach(function(todo){
+        //         todo.completed = true;
+        //     })
+        // }
+
+        this.todos.forEach(function(todo){
+            // Case 1: If everything's true, make everything false.
+            if (completedTodos === totalTodos) {
+                todo.completed = false;
+            // Case 2: Otherwise, make everything true
+
+            } else {
+                todo.completed = true;
             }
-            // Case 2: Otherwise, make everything true.
-        } else {
-            for (var i = 0; i < totalTodos; i++) {
-                this.todos[i].completed = true;
-            }
-        }
+        });
     }
 };
 var handlers = {
