83c862bcccafd2032113f7735fd95e4497dc8c5e
Nicole Luguesi <nicoleluguesi.student@careerdevs.com>
2019-09-01 20:39:55 -0400

    added toggles

--- todos.js
+++ todos.js
@@ -13,6 +13,7 @@ var todoList = {
                }
            }
        }
+    
     },
     addTodo: function(todoText) {
         this.todos.push({
@@ -20,4 +21,19 @@ var todoList = {
             completed: false
         });
         this.displayTodos();
-    }
+    },
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    },
+    toggleAll: function()
+};
